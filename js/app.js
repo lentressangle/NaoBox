69ca1ec21378bf7ee90a876ec54b4b2be6f15662
Loïc <loic_ent@yahoo.fr>
2015-12-03 15:49:03 +0100

    implement ng-click action to enter in dir

--- js/app.js
+++ js/app.js
@@ -4,10 +4,27 @@ app.controller('ListContentController', ['$scope','$window', '$http', ListConten
 
 
 function ListContentController($scope,$window,$http) {
-    adressEndpoint = "http://localhost:8000/api"
-    $http({ method: 'GET', url: adressEndpoint + '/content/root' }).
+
+    /*
+        GET content list of root directory
+    */
+    adressEndpoint = "http://localhost:8000/api/content/"
+    $http({ method: 'GET', url: adressEndpoint + 'root' }).
       success(function (data, status, headers, config) {
         console.log(data['data']['folder']);
         $scope.listFolder = data['data']['folder'];
       });
+
+    /*
+        GET content list of clicked directory
+    */
+    $scope.goTo = function(folder) {
+        console.log(folder);
+        $http({ method: 'GET', url: adressEndpoint + folder['relative_path'].substring(1) + "/" + folder['name'] }).success(function (data, status, headers, config) {
+            console.log(data['data']['folder']);
+            $scope.listFolder = data['data']['folder'];
+        });
+
+    }
+
 }
