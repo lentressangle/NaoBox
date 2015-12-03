52e9fd230f807eeb495813c8c02c1784ca1837ca
Loïc <loic_ent@yahoo.fr>
2015-12-03 17:51:22 +0100

    implement previous button

--- js/app.js
+++ js/app.js
@@ -8,23 +8,34 @@ function ListContentController($scope,$window,$http) {
     /*
         GET content list of root directory
     */
-    adressEndpoint = "http://localhost:8000/api/content/"
-    $http({ method: 'GET', url: adressEndpoint + 'root' }).
+    adressEndpoint = "http://localhost:8000/api/content"
+    $http({ method: 'GET', url: adressEndpoint + '/root' }).
       success(function (data, status, headers, config) {
-        console.log(data['data']['folder']);
         $scope.listFolder = data['data']['folder'];
+        $scope.current = "root";
+        $scope.root = true;
       });
 
     /*
         GET content list of clicked directory
     */
-    $scope.goTo = function(folder) {
-        console.log(folder);
-        $http({ method: 'GET', url: adressEndpoint + folder['relative_path'].substring(1) + "/" + folder['name'] }).success(function (data, status, headers, config) {
-            console.log(data['data']['folder']);
+    $scope.goTo = function(relative_path, name) {
+        adress = relative_path + name; // remove fisrt '/'
+        $http({ method: 'GET', url: adressEndpoint + adress}).success(function (data, status, headers, config) {
             $scope.listFolder = data['data']['folder'];
+            $scope.root = false;
+            $scope.parent_path = data['data']['folder'][0][0]['parent_path'];
+            if($scope.parent_path == "") {
+                $scope.parent_path = "/root";
+            }
         });
+    }
 
+    /*
+        GET content of parent folder
+    */
+    $scope.previous = function(parent_path) {
+        $scope.goTo(parent_path, '');
     }
 
 }
